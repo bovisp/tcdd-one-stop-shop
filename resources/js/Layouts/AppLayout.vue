a49207fed0968e4fe626e523ed242f2029ec7ba3
saeid <saeidrahmani81@gmail.com>
2022-03-31 17:52:11 -0400

    commented out media nav

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -44,12 +44,12 @@
                                 >
                                         {{ $t('moodle_catalogue') }}
                                 </jet-nav-link>
-                                <jet-nav-link
-                                        :href="route('moodle-media.index')"
-                                        :active="route().current('moodle-media.index')"
-                                >
-                                    {{ $t('moodle_media') }}
-                                </jet-nav-link>
+                                <!--<jet-nav-link-->
+                                        <!--:href="route('moodle-media.index')"-->
+                                        <!--:active="route().current('moodle-media.index')"-->
+                                <!--&gt;-->
+                                    <!--{{ $t('moodle_media') }}-->
+                                <!--</jet-nav-link>-->
                             </div>
                         </div>
 
